edaf1bcba28bcfbfcc16962b0cb3a80d528ddfb2
Jason Hammock <jhammock@protonmail.com>
2023-12-01 17:34:13 -0700

    more clean up

--- y2023/bin/main.ml
+++ y2023/bin/main.ml
@@ -17,7 +17,7 @@ let reverse s =
     |> List.to_seq
     |> String.of_seq
 
-let rec last_number s =
+let rec first_number s =
   let l = s 
     |> String.to_seq
     |> List.of_seq in 
@@ -26,22 +26,19 @@ let rec last_number s =
   | h :: rest -> 
     (match h with
     | '0' .. '9' -> h
-    | _ -> rest |> List.to_seq |> String.of_seq |> last_number)
+    | _ -> rest |> List.to_seq |> String.of_seq |> first_number)
 
-let first_number s =
-  s |> reverse |> last_number
+let last_number s =
+  s |> reverse
+    |> first_number
 
 let first_and_last s =
   let first = first_number s in
   let last = last_number s in
   [first; last]
-
-let char_of_list chars = 
-  chars 
-    |> List.rev
-    |> List.to_seq 
+    |> List.to_seq
     |> String.of_seq
-  
+
 let rec sum list =
   match list with
   | [] -> 0
@@ -51,7 +48,6 @@ let () =
   let file_name = Sys.argv.(1) in
   read_file file_name 
     |> List.map first_and_last
-    |> List.map char_of_list
     |> List.map int_of_string 
     |> sum
     |> string_of_int
